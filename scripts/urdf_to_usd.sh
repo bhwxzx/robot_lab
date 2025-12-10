21e994e68ef1f9dea549d5d54a6182539486da8f
lfr <12926148+jiandangbahuang@user.noreply.gitee.com>
2025-12-10 16:09:29 +0800

    添加一键urdf生成usd的脚本

---
+++ scripts/urdf_to_usd.sh
@@ -0,0 +1,6 @@
+python scripts/tools/convert_urdf.py \
+  source/robot_lab/data/Robots/LW/LW_description/LW.urdf \
+  source/robot_lab/data/Robots/LW/LW_description/LW.usd \
+  --joint-stiffness 0.0 \
+  --joint-damping 0.0 \
+  --joint-target-type position
