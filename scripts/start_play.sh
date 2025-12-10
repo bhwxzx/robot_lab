0a3614dd09c52652919a79c5e4426cc414814eed
lfr <12926148+jiandangbahuang@user.noreply.gitee.com>
2025-12-10 16:07:34 +0800

    添加一键play的脚本

---
+++ scripts/start_play.sh
@@ -0,0 +1,9 @@
+python scripts/reinforcement_learning/rsl_rl/play.py \
+    --task=RobotLab-Isaac-Velocity-Rough-LW-leg-teacher-Play-v0 \
+    --num_envs=500 \
+    # --keyboard \
+    # --real-time \
+    # --checkpoint=/home/young/liufengrong/legged_lab_v2/logs/rsl_rl/LW_leg_rough/2025-11-23_14-09-54/model_29999.pt
+    # --video \
+    # --video_length=2000 
+
