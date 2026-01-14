fcd42dc563573dfe1ef2f9cafe149139c64aac68
lfr <12926148+jiandangbahuang@user.noreply.gitee.com>
2026-01-14 22:06:12 +0800

    添加教师训练脚本

---
+++ scripts/start_teacher_train.sh
@@ -0,0 +1,7 @@
+python scripts/reinforcement_learning/rsl_rl/train.py \
+    --task=RobotLab-Isaac-Velocity-Rough-LW-leg-teacher-v0 \
+    --log_project_name=LW_leg_locomotion_teacher \
+    --logger=wandb \
+    --num_envs=4096 \
+    --headless \
+    # --resume \
