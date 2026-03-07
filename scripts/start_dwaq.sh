be7b2fd703aa1661aff692a2a6732143984561cf
lfr <12926148+jiandangbahuang@user.noreply.gitee.com>
2026-03-07 14:06:03 +0800

    脚本可以使用命令行传参

--- scripts/start_dwaq.sh
+++ scripts/start_dwaq.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
+
+# 设置默认值
+TERRAIN="Rough"
+type="leg"
+
+# 解析传入的命名参数
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --terrain) TERRAIN="$2"; shift ;;
+        --type) TYPE="$2"; shift ;;
+        *) echo "未知参数: $1"; exit 1 ;;
+    esac
+    shift
+done
+
 python scripts/reinforcement_learning/rsl_rl/train.py \
-    --task=RobotLab-Isaac-Velocity-Rough-LW-leg-Dwaq-v0 \
-    --log_project_name=LW_leg_locomotion_dwaq \
+    --task="RobotLab-Isaac-Velocity-${TERRAIN}-LW-${TYPE}-Dwaq-v0" \
+    --log_project_name="LW_${TYPE}_locomotion_dwaq" \
     --logger=wandb \
     --num_envs=4096 \
-    --headless \
-    # --resume \
+    --headless
